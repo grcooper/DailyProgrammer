65e74c5d85f5d0eccf545300db38356004290506
thadeaus <graham.cooper33@gmail.com>
2014-02-04 19:57:05 -0500

    WIP Falling Sand

---
+++ FallingSand/FallingSand/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FallingSand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = Convert.ToInt16(Console.ReadLine());
+
+            string[,] grid = new string[size, size];
+            string[] line = new string[size];
+            string[,] newGrid = new string[size, size];
+
+            
+
+            for (int i = 0; i < size; i++)
+            {
+                line = Console.ReadLine().Split();
+                for (int q = 0; q < size; q++)
+                {
+                    grid[i, q] = line[q];
+                }
+            }
+
+            for (int x = 0; x < size; x++)
+            {
+                for (int y = 0; y < size; y++)
+                {
+                    if (grid[x, y] == ".")
+                    {
+                        if (grid[x, y + 1] == "#")
+                        {
+                            newGrid[x, y] = ".";
+                        }
+                        else
+                        {
+                            grid[x, y + 1] = ".";
+                            newGrid[x, y] = " ";
+                        }
+                    }
+                    else if (grid[x, y] == "#")
+                    {
+                        newGrid[x, y] = "#";
+                    }
+                    else
+                    {
+                        newGrid[x, y] = " ";
+                    }
+                }
+            }
+            
+            for (int m = 0; m < size; m++)
+            {
+                for (int n = 0; n < size; n++)
+                {
+                    Console.WriteLine(newGrid[m, n]);
+                }
+            }
+            
+
+
+
+        }
+    }
+}
